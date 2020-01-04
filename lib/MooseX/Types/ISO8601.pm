0348df03e183eb4347f51184652e3a8e28a9d2cf
Karen Etheridge <ether@cpan.org>
2020-01-04 14:57:49 -0800

    increment $VERSION after 0.19 release

--- lib/MooseX/Types/ISO8601.pm
+++ lib/MooseX/Types/ISO8601.pm
@@ -1,7 +1,7 @@
 package MooseX::Types::ISO8601;
 # ABSTRACT: ISO8601 date and duration string type constraints and coercions for Moose
 
-our $VERSION = '0.19';
+our $VERSION = '0.20';
 
 use strict;
 use warnings;
